24fda1f68475656cd3622d4c36c1de2e5be6e481
Ashish Kalbhor <ashish.kalbhor@gmail.com>
2015-04-09 23:32:26 +0530

    Updated Reminder Action
    Can now set wallpaper.
    VibrateAction class removed.

--- RemindIt/src/com/ashish/remindit/ReminderAction.java
+++ RemindIt/src/com/ashish/remindit/ReminderAction.java
@@ -14,32 +14,53 @@
  */
 package com.ashish.remindit;
 
-import android.app.Service;
-import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.IBinder;
-import android.provider.ContactsContract;
-import android.provider.ContactsContract.CommonDataKinds;
-import android.text.format.Time;
-import android.widget.Toast;
+import java.io.IOException;
+import java.io.InputStream;
 
-/**
- * Trigger class to scan device contacts and match current date with contact birthday.
- * 
- * @author Ashish Kalbhor <ashish.kalbhor@gmail.com>
- * 
- */
+import android.app.Activity;
+import android.app.Service;
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.IBinder;
+import android.os.Vibrator;
+import android.util.DisplayMetrics;
+import android.widget.Toast;
 
 public class ReminderAction extends Service
 {
+	Vibrator vibe = null;
+	
 	@Override
 	public void onCreate()
 	{
-		VibrateAction vibrateAction = new VibrateAction();
+		this.vibe = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
 
 		Toast.makeText(getApplicationContext(), "REMINDER", Toast.LENGTH_SHORT).show();
-		vibrateAction.perform();
+		vibe.vibrate(1000);
+		
+		InputStream input = null;
+		try 
+		{
+			input = getAssets().open("image.jpg");
+			Bitmap bitmap = BitmapFactory.decodeStream(input);
+	
+		    //DisplayMetrics metrics = new DisplayMetrics(); 
+		    //((Activity)getBaseContext()).getWindowManager().getDefaultDisplay().getMetrics(metrics);
+		    // get the height and width of screen 
+		    //int height = metrics.heightPixels; 
+		    //int width = metrics.widthPixels;
+		           
+		    WallpaperManager wallpaperManager = WallpaperManager.getInstance(this); 
+		    wallpaperManager.setBitmap(bitmap);
+		    //wallpaperManager.suggestDesiredDimensions(width, height);
+		    Toast.makeText(this, "Wallpaper Set", Toast.LENGTH_SHORT).show();
+	    } catch (IOException e) 
+	    {
+	    	e.printStackTrace();
+	    }
 	}
 
 	@Override
